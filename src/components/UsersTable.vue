42f72848bfc68354fea7c08af6910f8b9e4edda7
Daria Mishalkina <dashaio251@gmail.com>
2023-03-28 20:53:04 +0200

    set publicPath in vue config

--- src/components/UsersTable.vue
+++ src/components/UsersTable.vue
@@ -34,7 +34,7 @@ let currentPageNumber = ref(1);
 let isUserFormVisible = ref(false);
 const getUsers = async() => {
   try {
-    const response = await axios.get("https://damishalkina.github.io/ue-spa-to-work-with-tables/db.json");
+    const response = await axios.get("https://damishalkina.github.io/vue-spa-to-work-with-tables/db.json");
     response.headers.common['Access-Control-Allow-Origin'] = '*';
     return response.data.users
   }
