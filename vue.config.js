734461aa22fa4587034af6b7820273954bbb3553
Daria Mishalkina <dashaio251@gmail.com>
2023-03-28 22:46:37 +0200

    vite config changes and router changes

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-// /* eslint-env node */
-// require('@rushstack/eslint-patch/modern-module-resolution')
-// module.exports = {
-//     publicPath: process.env.NODE_ENV === "production" ?
-//         "https://damishalkina.github.io/vue-spa-to-work-with-tables/"
-//         : "/"
-// }
