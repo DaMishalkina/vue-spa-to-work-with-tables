ab9741b2e14dd6504802de68a63a1b7b273a319d
Daria Mishalkina <dashaio251@gmail.com>
2023-03-28 22:38:21 +0200

    change base in vite config to fix gh-pages link

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ?
-      "https://damishalkina.github.io/vue-spa-to-work-with-tables/"
-      : "/",
+  // base: process.env.NODE_ENV === "production" ?
+  //     "https://damishalkina.github.io/vue-spa-to-work-with-tables/"
+  //     : "/",
+  base: "/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
