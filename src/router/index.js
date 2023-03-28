a498a620e697c2545568425dd34d274af7783de1
Daria Mishalkina <dashaio251@gmail.com>
2023-03-28 21:16:53 +0200

    fixing router for git pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 const router = createRouter({
-    history: createWebHistory("https://damishalkina.github.io/vue-spa-to-work-with-tables"),
+    history: createWebHistory("https://damishalkina.github.io/vue-spa-to-work-with-tables/"),
     routes: [
         {
             path: '/',
