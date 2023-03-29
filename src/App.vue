ebfa63fd48474ec29f6b24e9337790afc1ba0982
Daria Mishalkina <dashaio251@gmail.com>
2023-03-29 16:15:32 +0200

    delete unused components

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,8 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-<!--  <header>-->
-
-<!--    <div class="">-->
-<!--&lt;!&ndash;      <nav>&ndash;&gt;-->
-<!--&lt;!&ndash;        <RouterLink to="/">Home</RouterLink>&ndash;&gt;-->
-<!--&lt;!&ndash;        <RouterLink to="/about">About</RouterLink>&ndash;&gt;-->
-<!--&lt;!&ndash;      </nav>&ndash;&gt;-->
-<!--    </div>-->
-<!--  </header>-->
-
   <RouterView />
 </template>
 
@@ -48,52 +38,5 @@ import { RouterLink, RouterView } from 'vue-router'
   font-display: auto;
   unicode-range: U+000-5FF;
   src: local("Monsterrat"), url("./assets/fonts/Montserrat-Bold.ttf") format("truetype");
-}
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
