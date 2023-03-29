b76cd5b9dda8e1ded545f9125d15c1d645ab0c9e
Daria Mishalkina <dashaio251@gmail.com>
2023-03-29 12:18:35 +0200

    vite base changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 const router = createRouter({
-    history: createWebHistory(process.env.NODE_ENV === "production" ?
-        "https://damishalkina.github.io/vue-spa-to-work-with-tables/"
-        :import.meta.env.BASE_URL),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,9 +4,7 @@ import HomeView from '../views/HomeView.vue';
 
 
 const router = createRouter({
-  history: createWebHistory(process.env.NODE_ENV === "production" ?
-    "https://damishalkina.github.io/vue-spa-to-work-with-tables/"
-    :import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
